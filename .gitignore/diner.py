a2807dbf1625babfe43d211ae20500c63e3722c0
rorogant70 <ronagurl19@gmail.com>
2017-08-23 10:10:28 -0400

    Update and rename .gitignore to diner.py

---
+++ .gitignore/diner.py
@@ -0,0 +1,13 @@
+import random
+
+print(welcome to bistro gwc, Breakfeast, Lunch)
+Breakfeast= ["Fruit", "Waffles", "Coffee"]
+Lunch= ["Sandwich", "Chips", "Soda"]
+Dinner= ["Ravioli", "Chicken", "Water"]
+
+if input = Breakfeast
+    print(Breakfeast)
+if input= Lunch
+    print Lunch
+if  input= Dinner
+    print(Dinner)
